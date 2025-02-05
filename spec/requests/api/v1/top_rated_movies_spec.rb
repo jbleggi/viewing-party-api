c1676f69ce6be6c295f64df32124aff446a85ddc
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-02-05 16:13:52 -0500

    Create happy path RSpec tests

--- spec/requests/api/v1/top_rated_movies_spec.rb
+++ spec/requests/api/v1/top_rated_movies_spec.rb
@@ -1,5 +1,15 @@
-# bundle exec rspec spec/requests/top_rated_movies_spec.rb
+# bundle exec rspec spec/requests/api/v1/top_rated_movies_spec.rb
 require 'rails_helper'
 
-describe "Top Rated Movies ENDPOINT", type: :request do
+RSpec.describe "Top Rated Movies Endpoint", type: :request do
+    describe "happy path" do 
+        it "can retrieve top-rated movies from API" do
+        end
+
+        it "can retrieve a maximum of 20 results" do
+        end
+
+        it "includes the title and vote average of every movie" do
+        end    
+    end
 end
