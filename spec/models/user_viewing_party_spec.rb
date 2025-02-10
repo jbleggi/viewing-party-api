c413ce3d759949e23e84d72a0a89fbf491198b46
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-02-10 10:50:01 -0500

    Update User Viewing Party model spec. 2/2, 100% simplecov

--- spec/models/user_viewing_party_spec.rb
+++ spec/models/user_viewing_party_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UserViewingParty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should belong to User' do 
+    should belong_to(:user).with_foreign_key('invitee_id')    
+  end
+
+  it 'should belong to Viewing Party' do
+    should belong_to(:viewing_party)
+  end
 end
